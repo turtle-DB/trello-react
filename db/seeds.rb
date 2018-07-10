bc964a43f289461d19c339b19bbe5d4a4963db36
Steven Shen <steeve.shen@gmail.com>
2018-07-10 12:14:42 -0400

    added boards to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Boards.destroy_all
+
+board1 = Boards.create({title: 'board1'})
+board2 = Boards.create({title: 'board2'})
+board3 = Boards.create({title: 'board3'})
+board4 = Boards.create({title: 'board4'})
+board5 = Boards.create({title: 'board5'})
