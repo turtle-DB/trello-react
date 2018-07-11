a6e8b0ba20366ec7ab7eb85308cd78e1bea3a315
houstonfloyd <andrew.houstonfloyd#gmail.com>
2018-07-11 13:06:37 -0400

    Add BoardLists component
    Co-authored-by: Max Appleton <maxiappleton@gmail.com>
    Co-authored-by: Steven Shen <steeve.shen@gmail.com>

--- app/javascript/components/board/Board.jsx
+++ app/javascript/components/board/Board.jsx
@@ -4,11 +4,13 @@ import PropTypes from 'prop-types';
 import * as actions from '../../actions/BoardActions';
 
 import BoardHeader from './BoardHeader';
+import BoardLists from './BoardLists';
 
 function Board(props) {
   return (
     <div>
       <BoardHeader />
+      <BoardLists />
     </div>
   )
 }
